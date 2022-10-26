3005268759da7be9c8313b2715486ffd8c8cae9b
Bieleczki Gergő Péter <20d_bieleczkig@NYIRSZIKSZI.local>
2022-10-26 09:19:17 +0200

    Started java socket programming

---
+++ Java/sockets/src/test1.java
@@ -0,0 +1,21 @@
+import java.net.*;
+import java.io.*;
+
+
+public class test1 {
+
+    private Socket socket = null;
+    private DataInputStream input = null;
+    private DataOutputStream output = null;
+
+    public test1(String address, int port){
+    try {
+
+    }
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
