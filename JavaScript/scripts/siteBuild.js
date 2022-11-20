f4ce8f970c7c96218622cbeb33829af3288f6174
Tipikaly <bgergo8b@gmail.com>
2022-11-20 20:29:14 +0100

    Site works again, big changes are coming

--- JavaScript/scripts/siteBuild.js
+++ JavaScript/scripts/siteBuild.js
@@ -1,9 +1,32 @@
-function menuSect() {
-    let wrapper = document.createElement("div");
-    wrapper.className = "menu-section-wrapper";
-
-
+let siteBuild = {
+    imgWidth : 30,
+    imgHeight : 30,
+    wrapper : document.getElementById("menubar"),
 
 }
 
-let house = document.getElementById()
+function menuSect(src, dest, text) {
+    let wrapper = document.createElement("div");
+    wrapper.className = "menu-section-wrapper";
+
+    let icon = new Image();
+    icon.src = src;
+    icon.className = "menu-icon";
+    wrapper.appendChild(icon);
+
+    let p = document.createElement("p");
+    p.innerHTML = text;
+    p.className = "menu-text";
+    wrapper.appendChild(p);
+
+    return wrapper;
+}
+
+function createMenuElement(src, dest, text){
+    siteBuild.wrapper.appendChild(menuSect(src, dest, text));
+
+}
+
+createMenuElement("pictures/controller.svg", "games.html", "games");
+createMenuElement("pictures/test_tube.svg", "experiment.html", "experiments");
+createMenuElement("pictures/pencil-brush.svg", "canvas.html", "canvas");
